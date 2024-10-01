9f87877790b34ea1df90e9b8c981aac513c0ec82
vinicius.dev <72517355+viniciusdevegili@users.noreply.github.com>
2024-09-30 22:39:44 -0300

    alteração na estrutura do src

--- backend/src/controller/user.js
+++ backend/src/controller/user.js
@@ -1,47 +1,93 @@
-const UserModel = require('../model/user')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const user = require("../model/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const salts = 12;
+const SECRET_KEY = "exemplo";
+const SALT_VALUE = 10;
+
 class UserController {
-
-    async createUser(name, email, password) {
-        if (!name || !email || !password) {
-            throw new Error("Name, email e password são obrigatórios.")
-        }
-
-        const passwordHashed = bcrypt.hash(password, salts)
-
-        const userValue = await user.create({
-            name,
-            email,
-            password,
-        })
-        return userValue;
+  async createUser(nome, email, senha) {
+    if (!nome || !email || !senha) {
+      throw new Error("Nome, email e senha são obrigatórios.");
     }
 
-    findAll() {
-        return user.findAll();
+    const cypherSenha = await bcrypt.hash(String(senha), SALT_VALUE);
+
+    const userValue = await user.create({
+      nome,
+      email,
+      senha: cypherSenha,
+      permissao: "user"
+    });
+
+    return userValue;
+  }
+
+  async findUser(id) {
+    if (id === undefined) {
+      throw new Error("Id é obrigatório.");
     }
 
-    async login(email, password) {
-        const userLogged = await user.findOne({ where: { email }});
+    const userValue = await user.findByPk(id);
 
-        if(!userLogged) {
-            throw new Error("Email ou senha inválido. Tente novamente!")
-        }
-
-        const validPassword = userLogged.password === password;
-        
-        if(!validPassword) {
-            throw new Error("Email ou senha inválido. Tente novamente!")
-        }
-
-        return jwt.sign({ id: userLogged.id, email: userLogged.email },
-            'MeuSegredo123!'
-
-        )
+    if (!userValue) {
+      throw new Error("Usuário não encontrado.");
     }
+
+    return userValue;
+  }
+
+  async update(id, nome, email, senha) {
+    const oldUser = await user.findByPk(id);
+    if(email){
+      const sameEmail = await user.findOne({ where: { email } });
+      if (sameEmail && sameEmail.id !== id) {
+        throw new Error("Email já cadastrado.");
+      }
+    }
+    oldUser.nome = nome || oldUser.nome;
+    oldUser.email = email || oldUser.email;
+    oldUser.senha = senha
+      ? await bcrypt.hash(String(senha), SALT_VALUE)
+      : oldUser.senha;
+    oldUser.save();
+
+    return oldUser;
+  }
+
+  async delete(id) {
+    if (id === undefined) {
+      throw new Error("Id é obrigatório.");
+    }
+    const userValue = await this.findUser(id);
+    userValue.destroy();
+
+    return;
+  }
+
+  async find() {
+    return user.findAll();
+  }
+
+  async login(email, senha) {
+    if (email === undefined || senha === undefined) {
+      throw new Error("Email e senha são obrigatórios.");
+    }
+
+    const userValue = await user.findOne({ where: { email } });
+
+    if (!userValue) {
+      throw new Error("[1] Usuário e senha inválidos.");
+    }
+
+    const senhaValida = bcrypt.compare(String(senha), userValue.senha);
+    if (!senhaValida) {
+      throw new Error("[2] Usuário e senha inválidos.");
+    }
+
+    return jwt.sign({ id: userValue.id }, SECRET_KEY, { expiresIn: 60 * 60 });
+  }
+
 }
 
-module.exports = new UserController;
+module.exports = new UserController();
