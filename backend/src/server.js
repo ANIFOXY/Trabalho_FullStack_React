bf23edc8deec1270dc8804645cf392012d17fabe
viniciusdevegili <viniciusdevegili@hotmail.com>
2024-10-07 20:12:43 -0300

    att no server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,39 +1,34 @@
 const express = require("express");
 const cors = require("cors");
-const database = require("./config/database");
+const database = require("../src/config/database");
 
-const UserApi = require("./api/user");
-const UserRouter = require("./routes/user");
-const JokeRouter = require("./routes/joke");
-const authMiddleware = require("./middleware/auth");
+const UserRouter = require("../src/routes/user");
+const JokeRouter = require("../src/routes/joke");
+const UserApi = require("../src/api/user"); 
+const authMiddleware = require("../src/middleware/auth");
 
 const app = express();
-app.use(express.json());
-
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "OK" });
 });
 
 app.post("/api/login", UserApi.login);
-app.post("/api/user", UserApi.createUser);
+app.post("/api/user", UserApi.createUser); 
 
 app.use("/api/user", authMiddleware(), UserRouter);
 app.use("/api/joke", JokeRouter);
 
-
 database.db
-  .sync({ force: false })
-  .then((_) => {
-    if (!process.env.TEST) {
-      app.listen(3000, (_) => {
-        console.log("Server running on port 3000");
-      });
-    }
-  })
-  .catch((e) => {
-    console.error(`Erro ao inicializar o banco de dados ${e}`);
-  });
-
+    .sync({ force: false })
+    .then((_) => {
+        app.listen(3000, () => {
+            console.log("Servidor rodando na porta 3000")
+        });
+    })
+    .catch((e) => {
+        console.error("Erro ao conectar com o banco: ", e)
+    })
 module.exports = app;
