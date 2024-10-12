f05f5b0603fc82a48dd0ed8e025e1ea6d28ff87b
vinicius.dev <72517355+viniciusdevegili@users.noreply.github.com>
2024-10-12 12:18:20 -0300

    criação das rotas da joke na api do frontend

--- frontend/src/api/user.jsx
+++ frontend/src/api/user.jsx
@@ -1,26 +1,26 @@
 import api from './api'
 
 export const createUser = async (user) => {
-    const response = await api.post('/api/v1/user', user)
+    const response = await api.post('/api/user', user)
     return response.data
 }
 export const updateUser = async (id, user) => {
-    const response = await api.put(`/api/v1/user/${id}`, user)
+    const response = await api.put(`/api/user/${id}`, user)
     return response.data
 }
 export const deleteUser = async (id) => {
-    return api.delete(`/api/v1/user/${id}`)
+    return api.delete(`/api/user/${id}`)
 }
 export const getUserById = async (id) => {
-    const response = await api.get(`/api/v1/user/${id}`)
+    const response = await api.get(`/api/user/${id}`)
     return response.data
 }
 export const getUsers = async () => {
-    const response = await api.get(`/api/v1/user/`)
+    const response = await api.get(`/api/user/`)
     return response.data
 }
 export const loginUser = async (email, senha) => {
     const body = { email, senha }
-    const response = await api.post(`/api/v1/user`, body)
+    const response = await api.post(`/api/user`, body)
     return response.data
 }
