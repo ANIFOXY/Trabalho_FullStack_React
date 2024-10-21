517c46b73b07b1fa4d1de3b42276b6725cfc914f
Wesley Alan Arruda <wesley18alanarruda@gmail.com>
2024-10-21 20:25:29 -0300

    arrumando o front

--- frontend/src/api/user.jsx
+++ frontend/src/api/user.jsx
@@ -1,26 +1,36 @@
 import api from './api'
 
-export const createUser = async (user) => {
-    const response = await api.post('/api/v1/user', user)
+export const createUserAdmin = async (user) => {
+    console.log(user)
+    const response = await api.post('/api/user/admin', user)
+    console.log (response)
     return response.data
 }
+
+export const createUser = async (user) => {
+    console.log(user)
+    const response = await api.post('/api/user', user)
+    console.log (response)
+    return response.data
+}
+
 export const updateUser = async (id, user) => {
-    const response = await api.put(`/api/v1/user/${id}`, user)
+    const response = await api.put(`/api/user/${id}`, user)
     return response.data
 }
 export const deleteUser = async (id) => {
-    return api.delete(`/api/v1/user/${id}`)
+    return api.delete(`/api/user/${id}`)
 }
 export const getUserById = async (id) => {
-    const response = await api.get(`/api/v1/user/${id}`)
+    const response = await api.get(`/api/user/${id}`)
     return response.data
 }
 export const getUsers = async () => {
-    const response = await api.get(`/api/v1/user/`)
+    const response = await api.get(`/api/user/`)
     return response.data
 }
 export const loginUser = async (email, senha) => {
     const body = { email, senha }
-    const response = await api.post(`/api/v1/user`, body)
+    const response = await api.post(`/api/login`, body)
     return response.data
 }
