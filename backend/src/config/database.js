398b4ca05ffa9d8f17476d86b22fcf41b45838f4
viniciusdevegili <viniciusdevegili@hotmail.com>
2024-11-25 16:50:07 -0300

    docker_build

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -8,11 +8,11 @@ class Database {
     
     init(){
         this.db = new Sequelize({
-            database:"apiRenan",
-            host:"localhost",
-            password:"",
-            username:"root",
-            dialect:"mysql"
+            database: process.env.DB_DATABASE,
+            host: process.env.DB_HOST,
+            password: process.env.DB_PASSWORD,
+            username: process.env.DB_USER,
+            dialect: process.env.DB_DIALECT,
         })
     }
 }
